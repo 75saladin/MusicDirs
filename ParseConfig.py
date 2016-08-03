b47a95d6ca656a5360ac764495469f6037f8337f
Lucas Saladin <saladil197@potsdam.edu>
2016-08-03 09:48:35 -0400

    Cleaned up a few things, config argument can now have a leading 'use/' because people are going to do that

--- ParseConfig.py
+++ ParseConfig.py
@@ -6,7 +6,7 @@ from pathlib import Path
 configsLocation = os.path.dirname(__file__)+"use/"
 configArgIndex = 1
 useFlags = ["--help", "--use", "-h", "-u"]
-usageText="Use "+useFlags.__str__()+" to invoke this usage text.\n\nGive the name of a config file located in use/, with or without the trailing '.txt', to apply to the program's operations."
+usageText="Use "+useFlags.__str__()+" to invoke this usage text.\n\nGive the name of a config file located in use/, with or without the trailing '.txt' and leading 'use/', to apply to the program's operations."
     
 def parseCmd():
     """Parses and processes command-line arguments.
@@ -17,7 +17,7 @@ def parseCmd():
 
     flaged = False
     for arg in sys.argv:
-        #ie, if it's the first arg
+        #ie, if it's the program name
         if arg.endswith(".py"):
             continue
         
@@ -28,34 +28,41 @@ def parseCmd():
                 return None
             
         #If we get here, it's a config file
-        if not arg.endswith(".txt"):
-            cmdArg = arg+".txt"
-        f = open(configsLocation+cmdArg)
+        confArg = arg 
+        if not confArg.endswith(".txt"):
+            confArg = arg+".txt"
+        if confArg.startswith("use/"):
+            confArg = confArg[4:]
+            
+        f = open(configsLocation+confArg)
         theThingToReturn = parseConfig(f)
     
     #If we get here, args are done with no early returns. Return the thing.
     return theThingToReturn
 
 def parseConfig(f):
-    """Parses the file into meaningful dictionary.
+    """Parses the file into a meaningful dictionary.
     
     Example of returned dictionary: {'scheme': '%artist%/%album%/', 'cbase': 
     '/media/Storage/Music/archive', 'formats': {'mp3/320': 'portable/', 'flac':
     'flac/', 'mp3/192': 'portable190/'}}
     """
     config = {"formats":{}}
-    formats = {}
     
     for line in f:
         if line.startswith("//"): 
             continue
+        
         sline = re.split("[=\s]", line)
-        if sline[0] is not "":
-            if sline[0] is "format":
-                #Puts the format as a key in the dict pointed to by "formats"
-                config["formats"][sline[1]] = sline[3]                
-            else:
-                config[sline[0]] = sline[1]
+        if sline[0] is "":
+            continue
+        
+        if sline[0]=="format":
+            #Puts the format as a key in the dict pointed to by "formats"
+            config["formats"][sline[1]] = sline[3]                
+        else:
+            config[sline[0]] = sline[1]
+            
     return config
 
 ###############################################################################
